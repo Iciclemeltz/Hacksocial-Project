cde1379ee395efb6e5b70d54de76ef5efd7485b1
Lee Weichen <121931429+LeeWeiChen2200810@users.noreply.github.com>
2023-05-20 22:28:22 +0800

    Create appV2.py

---
+++ appV2.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+import dash
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import plotly.express as px
+
+df = pd.read_csv(r'Datasets\electricity_data.csv')
+df = df[1110:]
+df.reset_index(drop=True, inplace=True)
+df.index = pd.to_datetime(df['Date'])
+
+df2 = pd.read_csv(r'Datasets\electrical_appliance_consumption.csv')
+df2 = df2[df2['year'] == 2021]
+df2.reset_index(drop=True, inplace=True)
+
+df3 = pd.read_csv(r'Datasets\electrical_forecast.csv')
+
+df4 = pd.read_csv(r'Datasets\electricity_appliance_wise_data.csv')
+df4['Date'] = pd.to_datetime(df4['Date'])
+df4 = df4[df4['Date'].dt.year == 2021]
+df4.reset_index(drop=True, inplace=True)
+
+app = dash.Dash(__name__)
+app.title = 'Electra.AI'
+
+party_data = [
+    {'label': 'Time-Series Plot', 'value': 'Time-Series Plot'},
+    {'label': 'Appliance-wise Consumption', 'value': 'Appliance-wise Consumption'},
+    {'label': 'Electricity Consumption Forecast', 'value': 'Electricity Consumption Forecast'},
+    {'label': 'Faulty Devices', 'value': 'Faulty Devices'}
+]
+
+app.layout = html.Div(children=[
+    html.Div(className='row', children=[
+        html.Div(className='four columns div-user-controls', children=[
+            html.H2('Electra.AI Dashboard', style={'font-family': 'Trebuchet MS'}),
+            html.Div(className='div-for-dropdown', children=[
+                dcc.Dropdown(
+                    id='stockselector',
+                    options=party_data,
+                    value=['Time-Series Plot'],
+                    style={'backgroundColor': '#1E1E1E'},
+                    placeholder="Select an Option"
+                )
+            ], style={'color': '#1E1E1E'})
+        ]),
+        html.Div(className='eight columns div-for-charts bg-grey', children=[
+            dcc.Graph(id='timeseries', config={'displayModeBar': False})
+        ])
+    ])
+])
+
+
+@app.callback(Output('timeseries', 'figure'), [Input('stockselector', 'value')])
+def update_timeseries(selected_dropdown_value):
+    if selected_dropdown_value == 'Electricity Consumption Forecast':
+        df_sub = df3
+        df_anoms = df_sub[df_sub['MAE'] >= 15]
+        df_anoms.reset_index(drop=True, inplace=True)
+
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=df_sub['Date'], y=df_sub['Total_Consumption'], mode='lines',
+                                 name='Actual Consumption', line_color="#19E2C5"))
+        fig.add_trace(go.Scatter(x=df_sub['Date'], y=df_sub['Predicted_Consumption'], mode='lines',
+                                 name='Predicted Consumption', line_color="#C6810B"))
+        fig.add_trace(go.Scatter(x=df_anoms['Date'], y=df_anoms['Total_Consumption'], mode='markers',
+                                 name='Excess Consumption'))
+
+        fig.update_traces(marker=dict(size=5, line=dict(width=5, color='#C60B0B')))
+        fig.update_layout(
+            template='plotly_dark',
+            paper_bgcolor='rgba(0, 0, 0, 0)',
+            plot_bgcolor='rgba(0, 0, 0, 0)',
+            margin={'b': 15},
+            autosize
